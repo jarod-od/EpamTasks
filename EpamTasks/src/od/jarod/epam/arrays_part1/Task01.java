8e5fef2e8164ee46122507a8dd3eb88622338938
Jarod <konstantin.zalevsky@gmail.com>
2019-10-30 00:19:07 +0300

    arrays task

--- EpamTasks/src/od/jarod/epam/arrays_part1/Task01.java
+++ EpamTasks/src/od/jarod/epam/arrays_part1/Task01.java
@@ -4,19 +4,18 @@ package od.jarod.epam.arrays_part1;
 
 public class Task01 {
 	public static void main(String[] args) {
-		
-		int[] elem = {22, 33, 44, 55, 6, 77, 8, 9};
-		
+
+		int[] elem = { 22, 33, 44, 55, 6, 77, 8, 9 };
+
 		int sum = 0;
-		int divider = 2;
-		
+		int dividerK = 2;
+
 		for (int i = 0; i < elem.length; i++) {
-			
-			if (i%2 == 0) {
+
+			if (i % 2 == 0) {
 				sum = sum + elem[i];
 			}
 		}
 		System.out.println(sum);
-		
 	}
 }
