94168be7f69dac9247272219c0db33e6516defe2
Jarod <konstantin.zalevsky@gmail.com>
2019-11-01 00:14:10 +0300

    arrays simple

--- EpamTasks/src/od/jarod/epam/arrays_part1/Task02.java
+++ EpamTasks/src/od/jarod/epam/arrays_part1/Task02.java
@@ -6,11 +6,23 @@ package od.jarod.epam.arrays_part1;
 */
 
 public class Task02 {
-
 	public static void main(String[] args) {
 		
+		int[] elem = {0, 33, 23, 0, 87, 0, 4,-3};
 		
+		int counterZeroNumbers = 0;
 		
+		for (int i = 0; i < elem.length; i++) {
+			if (elem[i] == 0) {
+				counterZeroNumbers++;
+//				System.out.println(elem[i]);
+			}	
+		}
+		System.out.println(counterZeroNumbers);
 		
+		int[] zeroNumbers = new int[counterZeroNumbers];
+		for (int i = 0; i < zeroNumbers.length; i++) {
+			System.out.println(zeroNumbers[i]);
+		}
 	}
 }
