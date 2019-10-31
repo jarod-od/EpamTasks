b0297904c42752ec8670d9b24f32dbebbdd92d0a
Jarod <konstantin.zalevsky@gmail.com>
2019-11-01 00:20:21 +0300

    task

---
+++ EpamTasks/src/od/jarod/epam/arrays_part1/Task03.java
@@ -0,0 +1,14 @@
+package od.jarod.epam.arrays_part1;
+
+//Дана последовательность целых чисел. Выяснить, какое число встречается раньше 
+//	- положительное или отрицательное
+
+public class Task03 {
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
