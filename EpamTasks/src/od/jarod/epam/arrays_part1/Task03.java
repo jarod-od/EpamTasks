6f21b10dd00f2860c8df028fb2239c6712370aa1
Jarod <konstantin.zalevsky@gmail.com>
2019-11-05 23:43:58 +0200

    arrays

--- EpamTasks/src/od/jarod/epam/arrays_part1/Task03.java
+++ EpamTasks/src/od/jarod/epam/arrays_part1/Task03.java
@@ -5,10 +5,21 @@ package od.jarod.epam.arrays_part1;
 
 public class Task03 {
 	public static void main(String[] args) {
-		
-		
-		
-		
-	}
 
+		int[] numbers = { 3, 4, -5, 8, -45 };
+
+		String marker = " ";
+		for (int i = 0; i < numbers.length;) {
+
+			if (numbers[i] < 0) {
+				marker = "First element is negative: " + numbers[i];
+				break;
+
+			} else {
+				marker = "First element is positive: " + numbers[i];
+				break;
+			}
+		}
+		System.out.println(marker);
+	}
 }
